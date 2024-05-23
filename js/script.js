8989741bbd969392320228f71ebea6ba866e670e
MirkoValle <mirko.valle20@gmail.com>
2024-05-23 18:06:14 +0200

    Added student list for snack 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// SNACK 1
 const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 let guestsList = []
 
@@ -12,3 +13,15 @@ guests.forEach((guest, index) => {
 });
 
 console.log(guestsList);
+
+
+//SNACK 2 
+const students = [
+    { id: 213, name: "Giuseppina della rovere", grades: 78 },
+    { id: 110, name: "Paola Cortellessa", grades: 96 },
+    { id: 250, name: "Andrea mantegna", grades: 48 },
+    { id: 145, name: "Gaia Borromini", grades: 74 },
+    { id: 196, name: "Luigi Grimaldello", grades: 68 },
+    { id: 102, name: "Piero della Francesca", grades: 50 },
+    { id: 120, name: "Francesca da Polenta", grades: 84 }
+];
